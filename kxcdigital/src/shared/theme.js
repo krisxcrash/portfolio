b0d68439882e21e8c68004a22c01f574b5572f1f
Kristine White <kwhite@happymoney.com>
2019-08-23 21:57:02 -0700

    updated initial skeleton

---
+++ kxcdigital/src/shared/theme.js
@@ -0,0 +1,13 @@
+const colors = {
+  primary: '#00FFE1',
+  secondary: '#f4f4f4',
+  black: '#000000',
+  white: '#ffffff',
+  darkGrey: '#918F90'
+}, 
+const fonts = {
+  fontHeadline: '',
+  fontBody: ''
+}
+
+export default theme
