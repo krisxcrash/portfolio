42ca662bc1e4d1a5759a684e2f4b438403bf7d0c
Kristine White <kwhite@happymoney.com>
2020-03-16 22:09:09 -0700

    initial file setup

---
+++ portfolio/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from "../components/header"
+
+export default () => (
+  <div>
+    <Header headerText="About Header" />
+    <h1>About</h1>
+  </div>
+)
---
+++ portfolio/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import {Link} from 'gatsby'
+
+export default () => (<div>
+  <h1>Hello there!</h1>
+  <Link to="/about/">About</Link>
+  </div>)
